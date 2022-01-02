8e251c1494ff946d68b058670dfddc69c903cbf1
Satya-Adhi <muhammad1900016100@webmail.uad.ac.id>
2022-01-02 18:35:48 +0700

    Co-authored-by: Abhysta <Abhysta@users.noreply.github.com> Co-authored-by: ragilmustaqim23 <ragilmustaqim23@users.noreply.github.com>

---
+++ application/controllers/owner.php
@@ -0,0 +1,49 @@
+<?php 
+
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Controllername extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Load Dependencies
+
+    }
+
+    // List all your items
+    public function index( $offset = 0 )
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data ['judul']= 'Halaman Administrator';
+       $this->load->view('template/header',$data);
+       $this->load->view('owner/dashboard');
+       $this->load->view('template/footer');
+    }
+
+    // Add a new item
+    public function add()
+    {
+
+    }
+
+    //Update one item
+    public function update( $id = NULL )
+    {
+
+    }
+
+    //Delete one item
+    public function delete( $id = NULL )
+    {
+
+    }
+}
+
+/* End of file Controllername.php */
+
+
+
+?>
